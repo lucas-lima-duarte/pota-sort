796065d5349faa0258ba4af7ae5eedfe4a1eeb81
avantasy <lucas_16_lima@hotmail.com>
2019-10-17 09:21:37 -0300

    Começando do 0

--- App.java
+++ App.java
@@ -1,14 +1,6 @@
-
 public class App {
     public static void main(String[] args) {
-
-        int [] vetorTeste = {1,2,3,4,5,6,9,8};
-        SelectionSort s1 = new SelectionSort();
-        s1.Ordenar(vetorTeste);
-        for (int e : vetorTeste) {
-            System.out.println(e);
-        }
-
-     System.out.println("Contabilizadas " + s1.getContador() + " comparação(ções)");
+        System.out.println("Ola mundo");
     }
+
 }
