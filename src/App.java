43cee28d096ed731590e0e881dc2f15fd08a9cf5
avantasy <lucas_16_lima@hotmail.com>
2019-10-21 18:33:15 -0300

    App

--- src/App.java
+++ src/App.java
@@ -1,14 +1,13 @@
 public class App {
     public static void main(String[] args) {
         int[] vetor = { 10, 9, 7, 8, 5, 2, 1 };
+
         Insertion i = new Insertion();
 
         i.Ordenar(vetor);
 
         i.mostraVetor(vetor);
 
-        System.out.println(i.getComparacoes());
-        System.out.println(i.getTrocas());
 
     }
 
