744bf39afe939b2d0e4500fa9275ae65fc2b587e
avantasy <lucas_16_lima@hotmail.com>
2019-10-22 09:57:23 -0300

    Merge Sort completa

--- src/App.java
+++ src/App.java
@@ -1,12 +1,16 @@
 public class App {
     public static void main(String[] args) {
-        int[] vetor = { 10, 9, 7, 8, 5, 2, 1 };
+        int [] vetor = { 10, 9, 7, 8, 5, 2, 1 };        
 
-        Insertion i = new Insertion();
+        Merge teste = new Merge();
 
-        i.Ordenar(vetor);
+        teste.Ordenar(vetor, 0, (vetor.length - 1));;
 
-        i.mostraVetor(vetor);
+        teste.mostraVetor(vetor);
+
+        System.out.println(teste.getComparacoes());
+
+        
 
 
     }
--- src/Merge.java
+++ src/Merge.java
@@ -1,11 +1,41 @@
 public class Merge extends Sort {
 
-    public  Merge() {
+    public Merge(){
         super();
     }
-
-    @Override
-    public void Ordenar(int [] vetor) {
-        
+    
+    public void Ordenar(int [] vetor, int inicio, int fim) {
+        if (inicio < fim) {
+            int meio = (inicio + fim) / 2;
+            Ordenar(vetor, inicio, meio);
+            Ordenar(vetor, meio + 1, fim);
+            Intercala(vetor, inicio, meio, fim);
+        }
     }
-}
+
+    public void Intercala(int[] A, int p, int q, int r) {
+        int B[] = new int[A.length];
+        for (int k = p; k <= r; k++) {
+            B[k] = A[k];
+            incrementaComparacoes();
+        }
+            int i = p;
+            int j = q+1;
+            for (int k = p; k <= r; k++) {
+                if (i > q) {
+                    A[k] = B[j++];
+                    incrementaComparacoes();
+                } else if (j > r){
+                    A[k] = B[i++];
+                    incrementaComparacoes();
+                } else if (B[i] < B[j]){
+                    A[k] = B[i++];
+                    incrementaComparacoes();
+                } else {
+                    A[k] = B[j++];
+                    incrementaComparacoes();
+                }
+                incrementaComparacoes();
+            }
+        }
+    }
