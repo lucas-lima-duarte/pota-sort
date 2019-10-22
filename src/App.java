1042871b3c43ca3a3b59e0b3e9024b5149d11b0d
avantasy <lucas_16_lima@hotmail.com>
2019-10-22 10:17:22 -0300

    count Sort feito

--- src/App.java
+++ src/App.java
@@ -1,18 +1,6 @@
 public class App {
     public static void main(String[] args) {
         int [] vetor = { 10, 9, 7, 8, 5, 2, 1 };        
-
-        Merge teste = new Merge();
-
-        teste.Ordenar(vetor, 0, (vetor.length - 1));;
-
-        teste.mostraVetor(vetor);
-
-        System.out.println(teste.getComparacoes());
-
-        
-
-
     }
 
 }
--- src/Merge.java
+++ src/Merge.java
@@ -1,14 +1,14 @@
 public class Merge extends Sort {
 
-    public Merge(){
+    public Merge() {
         super();
     }
-    
-    public void Ordenar(int [] vetor, int inicio, int fim) {
+
+    public void MergeSort(int[] vetor, int inicio, int fim) {
         if (inicio < fim) {
             int meio = (inicio + fim) / 2;
-            Ordenar(vetor, inicio, meio);
-            Ordenar(vetor, meio + 1, fim);
+            MergeSort(vetor, inicio, meio);
+            MergeSort(vetor, meio + 1, fim);
             Intercala(vetor, inicio, meio, fim);
         }
     }
@@ -19,23 +19,28 @@ public class Merge extends Sort {
             B[k] = A[k];
             incrementaComparacoes();
         }
-            int i = p;
-            int j = q+1;
-            for (int k = p; k <= r; k++) {
-                if (i > q) {
-                    A[k] = B[j++];
-                    incrementaComparacoes();
-                } else if (j > r){
-                    A[k] = B[i++];
-                    incrementaComparacoes();
-                } else if (B[i] < B[j]){
-                    A[k] = B[i++];
-                    incrementaComparacoes();
-                } else {
-                    A[k] = B[j++];
-                    incrementaComparacoes();
-                }
+        int i = p;
+        int j = q + 1;
+        for (int k = p; k <= r; k++) {
+            if (i > q) {
+                A[k] = B[j++];
+                incrementaComparacoes();
+            } else if (j > r) {
+                A[k] = B[i++];
+                incrementaComparacoes();
+            } else if (B[i] < B[j]) {
+                A[k] = B[i++];
+                incrementaComparacoes();
+            } else {
+                A[k] = B[j++];
                 incrementaComparacoes();
             }
+            incrementaComparacoes();
         }
     }
+    
+    @Override
+    public void Ordenar(int[] vetor) {
+        MergeSort(vetor, 0, vetor.length - 1);
+    }
+}
